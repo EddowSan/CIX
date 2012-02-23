b57a6d4d6c09e8faef103c5dd71c50db3df19c6d
Will <wwhitmore92@yahoo.com>
2012-02-23 12:12:27 -0800

    Authentication

---
+++ NewsWriter/NewsFeedInput/Login.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="NewsFeedInput.Login" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login Page</title>
+    <script type="text/javascript">
+        function pressed() {
+            var element = document.getElementById("btnSubmit");
+            element.style.backgroundColor = "#2354A9";
+        }
+
+        function released() {
+            var element = document.getElementById("btnSubmit");
+            element.style.backgroundColor = "#3465BA";
+        }
+    </script>
+</head>
+<body>
+    <div class="app">
+        <div class="header">
+            <img src="Images/logo.png" alt="Coleman Logo" />
+            <h1 runat="server" id="headerTag">Coleman University<br />Welcome to the News Writer!</h1>
+        </div>
+        <form id="form1" runat="server">
+            <div class="content">
+                <p>
+                    <asp:Label ID="lblUserName" runat="server" Text="Username: "/>
+                    <asp:TextBox ID="txtUserName" runat="server" />
+                    <asp:RequiredFieldValidator ID="valUserName" runat="server" 
+                        ErrorMessage="Username Needed!" ControlToValidate="txtUserName" />
+                </p>
+                <p>
+                    <asp:Label ID="lblPassword" runat="server" Text="Password: " />
+                    <asp:TextBox ID="txtPassword" runat="server" />
+                    <asp:RequiredFieldValidator ID="valPassword" runat="server" 
+                        ErrorMessage="Password Needed!" ControlToValidate="txtPassword" />
+                </p>
+                <p>
+                    <asp:Button ID="btnSubmit" runat="server" Text="Submit" onmousedown="pressed()" onmouseup="released()" onmouseout="released()" />
+                </p>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ NewsWriter/NewsFeedInput/Login.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace NewsFeedInput
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack == true)
+            {
+                ValidateUser();
+            }
+        }
+
+        public bool ValidateUser()
+        {
+            MySqlConnection conn;
+            MySqlCommand comm;
+            string passwordLookUp = null;
+
+            if ((null == txtUserName.Text) || (0 == txtUserName.Text.Length) || (txtUserName.Text.Length > 15))
+            {
+                headerTag.InnerHtml = "Coleman University<br />UserName Failed";
+                return false;
+            }
+
+            if ((null == txtPassword.Text) || (0 == txtPassword.Text.Length) || (txtPassword.Text.Length > 25))
+            {
+                headerTag.InnerHtml = "Coleman University<br />Password Failed";
+                return false;
+            }
+
+            string strConnection = ConfigurationManager.ConnectionStrings["NewsWriter"].ConnectionString;
+            
+            conn = new MySqlConnection(strConnection);
+
+            comm = new MySqlCommand("SELECT Password FROM Users WHERE UserName = @UserName", conn);
+            comm.Parameters.Add("@UserName", MySqlDbType.VarChar);
+            comm.Parameters["@UserName"].Value = txtUserName;
+
+            try
+            {
+                conn.Open();
+
+                passwordLookUp = (string)comm.ExecuteScalar();
+            }
+            catch (Exception error)
+            {
+                headerTag.InnerHtml = "Coleman University<br />" + error.Message;
+                return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            if (null == passwordLookUp)
+            {
+                headerTag.InnerHtml = "Coleman University<br />No Matching User";
+                return false;
+            }
+
+            // Compare lookupPassword and input passWord, using a case-sensitive comparison.
+            return (0 == string.Compare(passwordLookUp, txtPassword.Text, false));
+        }
+    }
+}
